85dd1b9048d85919298e19c62db370fb644e4b7a
andreapiscioneri <andrypiscioneri@gmail.com>
2023-12-24 11:42:30 +0100

    first commit

---
+++ javascript/slideshow.js
@@ -0,0 +1,17 @@
+var slider_img = document.getElementById('slide');
+var images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg'];
+var i = 0;
+
+function prev() {
+  i = (i - 1 + images.length) % images.length;
+  setImg();
+}
+
+function next() {
+  i = (i + 1) % images.length;
+  setImg();
+}
+
+function setImg() {
+  slider_img.src = "/Images/" + images[i];
+}
